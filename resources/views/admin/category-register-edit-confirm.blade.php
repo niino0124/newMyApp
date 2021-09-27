c7951db9e2ce0fdace342211bbd5849056b96140
niino0124 <niino@selva-i.co.jp>
2021-09-27 12:16:15 +0900

    confirmの一元化はした

--- resources/views/admin/category-register-edit-confirm.blade.php
+++ resources/views/admin/category-register-edit-confirm.blade.php
@@ -4,7 +4,7 @@
 <div class="blue-board_admin">
     <div class="header_b">
         <div class="simple-wrap_sb">
-            <p class="fw-bold">@if($input["id"] != null)
+            <p class="fw-bold">@if(isset($input["id"]))
                 商品カテゴリ編集確認
             @else 商品カテゴリ登録確認 @endif</p>
             <div class="simple-wrap">
@@ -15,97 +15,70 @@
         </div>
     </div>
     <div class="man-body_b">
-        @if ($input["id"] != null)
-        <form method="post" action="{{route('admin.category-edit-complete')}}">
+        <form method="post" action="@if(isset($input["id"]))@endif{{route('admin.category-edit-complete')}}@else{{route('admin.category-register-complete')}}">
             @csrf
             <div class="element_wrap_str">
                 <label>商品大カテゴリID</label>
-                <p>{{ $input["id"] }}</p>
+                <p>@if(isset($input["id"])){{ $input["id"] }}@else 登録後に自動採番 @endif</p><input value="{{ $input["id"] }}" name="id" hidden>
             </div>
             <div class="element_wrap_str">
                 <label>商品大カテゴリ</label>
-                <p>{{ $input["name"] }}</p>
+                <p>{{ $input["name"] }}</p><input value="{{ $input["name"] }}" name="name" hidden>
+
             </div>
 
             <div class="element_wrap_str">
                 <label>商品小カテゴリ</label>
                 <div class="content-wrap">
-                    <p class="sub_name tal">{{ $input["sub_name0"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name0"] }}</p>            <input value="{{ $input["sub_name0"] }}" name="sub_name0" hidden>
+
 
                     @if ($input["sub_name1"])
-                    <p class="sub_name tal">{{ $input["sub_name1"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name1"] }}</p>            <input value="{{ $input["sub_name1"] }}" name="sub_name1" hidden>
+
                     @endif
                     @if ($input["sub_name2"])
-                    <p class="sub_name tal">{{ $input["sub_name2"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name2"] }}</p>            <input value="{{ $input["sub_name2"] }}" name="sub_name2" hidden>
+
                     @endif
                     @if ($input["sub_name3"])
-                    <p class="sub_name tal">{{ $input["sub_name3"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name3"] }}</p>            <input value="{{ $input["sub_name3"] }}" name="sub_name3" hidden>
+
                     @endif
                     @if ($input["sub_name4"])
-                    <p class="sub_name tal">{{ $input["sub_name4"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name4"] }}</p>            <input value="{{ $input["sub_name4"] }}" name="sub_name4" hidden>
+
                     @endif
                     @if ($input["sub_name5"])
-                    <p class="sub_name tal">{{ $input["sub_name5"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name5"] }}</p>            <input value="{{ $input["sub_name5"] }}" name="sub_name5" hidden>
+
                     @endif
                     @if ($input["sub_name6"])
-                    <p class="sub_name tal">{{ $input["sub_name6"] }}</p>
+                    <p class="sub_name tal">{{ $input["sub_name6"] }}</p>            <input value="{{ $input["sub_name6"] }}" name="sub_name6" hidden>
+
                     @endif
                     @if ($input["sub_name7"])
                     <p class="sub_name tal">{{ $input["sub_name7"] }}</p>
+                    <input value="{{ $input["sub_name7"] }}" name="sub_name7" hidden>
                     @endif
                     @if ($input["sub_name8"])
                     <p class="sub_name tal">{{ $input["sub_name8"] }}</p>
+                    <input value="{{ $input["sub_name8"] }}" name="sub_name8" hidden>
                     @endif
                     @if ($input["sub_name9"])
                     <p class="sub_name tal">{{ $input["sub_name9"] }}</p>
+                    <input value="{{ $input["sub_name9"] }}" name="sub_name9" hidden>
                     @endif
                 </div>
-
             </div>
 
 
-            <input value="{{ $input["id"] }}" name="id" hidden>
-            <input value="{{ $input["name"] }}" name="name" hidden>
-            <input value="{{ $input["sub_name0"] }}" name="sub_name0" hidden>
-            <input value="{{ $input["sub_name1"] }}" name="sub_name1" hidden>
-            <input value="{{ $input["sub_name2"] }}" name="sub_name2" hidden>
-            <input value="{{ $input["sub_name3"] }}" name="sub_name3" hidden>
-            <input value="{{ $input["sub_name4"] }}" name="sub_name4" hidden>
-            <input value="{{ $input["sub_name5"] }}" name="sub_name5" hidden>
-            <input value="{{ $input["sub_name6"] }}" name="sub_name6" hidden>
-            <input value="{{ $input["sub_name7"] }}" name="sub_name7" hidden>
-            <input value="{{ $input["sub_name8"] }}" name="sub_name8" hidden>
-            <input value="{{ $input["sub_name9"] }}" name="sub_name9" hidden>
-
-            {{-- ↑nullが紛れている。コントローラでnull以外！みたいな強力なものがあるかな？ --}}
             <div class="btn-wrap">
-                <input type="submit" class="btn-back_b" value="編集完了" />
+                @if(isset($input["id"]))<input type="submit" class="btn-back_b" value="編集完了" />@else<input type="submit" class="btn-back_b" value="登録完了" /> @endif
                 <input href="" class="btn_b" name="back" value="前に戻る" type="submit">
             </div>
+
         </form>
-
-
-        @else
-        {{-- actionに関して@if @else で書き換えできると思うけど --}}
-        <form method="post" action="{{route('admin.category-register-complete')}}">
-            @csrf
-            <div class="element_wrap_str">
-                <label>ID</label>
-                <p>登録後に自動採番</p>
-            </div>
-
-
-
-
-
-            <div class="btn-wrap">
-                <input type="submit" class="btn-back_b" value="登録完了" />
-                <input href="{{url()->previous()}}" class="btn_b" name="back" value="前に戻る" type="submit">
-
-            </div>
-        </form>
-        @endif
-
     </div>
 </div>
 @endsection
